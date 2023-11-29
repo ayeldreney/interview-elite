8ebe3d878c758dbd3e6fe954e6c13e384b0dc216
ayeldreney <60024298+ayeldreney@users.noreply.github.com>
2023-11-29 13:22:15 +0200

    initial commit

---
+++ Dal/models/item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace inteview_task.DAL.models
+{
+	
+	public class item
+	{
+		public int ID { get; set; }
+		public string Item_name { get; set; }
+		public int Quntity { get; set; }
+		public int Unit_price { get; set; }
+
+		public int Totat { get; set; }
+	}
+}
---
+++ main/js/invoice-create.js
@@ -0,0 +1,64 @@
+﻿
+      
+        var parentAll = document.querySelectorAll('.invoice-details-table tbody tr').forEach(el => {
+        let textFields = el.querySelectorAll(".muted-text input[type=text]");
+    console.log(textFields);
+    let qty = textFields[0];
+    let price = textFields[1];
+    if (qty == undefined || price == undefined) return;
+    let totalFieldPriceSpan = el.querySelector('.text-primary span');
+
+            qty.addEventListener('input', e => {
+                if (isNaN(parseInt(qty.value)) || isNaN(parseInt(price.value))) {
+        totalFieldPriceSpan.innerHTML = 0
+    };
+               
+
+                if (price.value > 0 && qty.value > 0) {
+        totalFieldPriceSpan.innerHTML = price.value * qty.value;
+                }
+    updateNet()
+            });
+
+            price.addEventListener('input', e => {
+                if (isNaN(parseInt(qty.value))) return;
+    if (isNaN(parseInt(price.value))) return;
+
+                if (price.value > 0 && qty.value > 0) {
+        totalFieldPriceSpan.innerHTML = price.value * qty.value;
+                }
+    updateNet()
+            });
+        });
+
+
+
+
+
+    function updateNet() {
+            var total = 0;
+            var parentAll = document.querySelectorAll('.invoice-details-table tbody tr .text-primary span:not(#totalPrice) ').forEach(el => {
+        console.log(el);
+    total += parseInt(el.innerHTML);
+            })
+
+    document.querySelector("#totalPrice").innerHTML = total;
+         
+
+        }
+
+    function deleteCheckedRows() {
+
+        document.querySelectorAll(".invoice-details-table tbody tr").forEach(e => {
+            if (e.querySelector("input[type=checkbox]").checked) {
+                e.remove()
+                updateNet()
+            }
+        })
+
+
+
+    }
+
+
+
